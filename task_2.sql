bc782e248f9097ce8b1ee184e5e6774a0fb58ed6
uservkai <katemary6623@gmail.com>
2025-08-17 20:25:33 +0300

    create sql tables with attributes

---
+++ task_2.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR (215) NOT NULL
+);
+
+-- Create customers table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
+);
+
+-- Create books table
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    publication_date DATE,
+    price DOUBLE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Create orders table
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+
+);
+
+-- Create order_details table
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
